74c22fa57916ee6de99c44d516f7c988514aafdf
Zeke Hammonds <zaiah0703@yahoo.com>
2018-12-09 15:32:58 -0500

    removed unecessary code, added walking animation

--- Final Project/HW1/HW1.cpp
+++ Final Project/HW1/HW1.cpp
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
 #ifdef _WIN32
 #include <windows.h>
 #endif
@@ -20,20 +17,14 @@ void usleep(unsigned int nanosec)
 }
 #endif
 
-int WINDOW_WIDTH = 400;
-int WINDOW_HEIGHT = 400;
-float rotateLeftArm = 0.0f;
-float rotateRightArm = 0.0f;
-float rotateHead = 0.0f;
-const float PI = 3.14159;
-
-float joint_rotLARM = 0.0f;
-float joint_rotRARM = 0.0f;
-float joint_rotLLEG = 0.0f;
-float joint_rotRLEG = 0.0f;
+//float rotateLeftArm = 0.0f;
+//float rotateRightArm = 0.0f;
+//float rotateHead = 0.0f;
 
 
 // Window settings
+int WINDOW_WIDTH = 400;
+int WINDOW_HEIGHT = 400;
 int windowID;               // Glut window ID (for display)
 GLUI *glui;                 // Glui window (for controls)
 int Win[2];                 // window (x,y) size
@@ -44,12 +35,12 @@ int animate_mode = 0;       // 0 = no anim, 1 = animate
 int animation_frame = 0;      // Specify current frame of animation
 
 // Joint parameters
-const float JOINT_MIN = 0.0f;
-const float JOINT_MAX = 360.0f;
-float joint_rot = 0.0f;
-float joint_x = 0;
-float joint_y = 0;
-
+const float JOINT_MIN = -45.0f;
+const float JOINT_MAX = 45.0f;
+float joint_rotLARM = 0.0f;
+float joint_rotRARM = 0.0f;
+float joint_rotLLEG = 0.0f;
+float joint_rotRLEG = 0.0f;
 
 
 enum {
@@ -69,23 +60,6 @@ GLfloat red_light[] =
 GLfloat left_light_position[] =
 { -1.0, 0.0, 1.0, 0.0 }, right_light_position[] =
 { 1.0, 0.0, 1.0, 0.0 };
-GLfloat brass_ambient[] =
-{ 0.33, 0.22, 0.03, 1.0 }, brass_diffuse[] =
-{ 0.78, 0.57, 0.11, 1.0 }, brass_specular[] =
-{ 0.99, 0.91, 0.81, 1.0 }, brass_shininess = 27.8;
-GLfloat red_plastic_ambient[] =
-{ 0.0, 0.0, 0.0 }, red_plastic_diffuse[] =
-{ 0.5, 0.0, 0.0 }, red_plastic_specular[] =
-{ 0.7, 0.6, 0.6 }, red_plastic_shininess = 32.0;
-GLfloat emerald_ambient[] =
-{ 0.0215, 0.1745, 0.0215 }, emerald_diffuse[] =
-{ 0.07568, 0.61424, 0.07568 }, emerald_specular[] =
-{ 0.633, 0.727811, 0.633 }, emerald_shininess = 76.8;
-GLfloat slate_ambient[] =
-{ 0.02, 0.02, 0.02 }, slate_diffuse[] =
-{ 0.02, 0.01, 0.01 }, slate_specular[] =
-{ 0.4, 0.4, 0.4 }, slate_shininess = .78125;
-
 int shade_model = GL_SMOOTH;
 char *left_light, *right_light;
 char *ico_material, *teapot_material, *torus_material;
@@ -93,14 +67,21 @@ char *ico_material, *teapot_material, *torus_material;
 void animate()
 {
 	// Update geometry
-	const double joint_rot_speed = 10;
-	double joint_rot_t = double(int(animation_frame*joint_rot_speed) % int(JOINT_MAX)) / JOINT_MAX;
-	joint_rot = joint_rot_t * JOINT_MIN + (1 - joint_rot_t) * JOINT_MAX;
+	const double joint_rot_speed = 2;
+	double joint_rotLARM_t = double(int(animation_frame*joint_rot_speed) % int(JOINT_MAX)) / JOINT_MAX;
+	joint_rotLARM += joint_rotLARM_t * JOINT_MIN + (1 - joint_rotLARM_t) * JOINT_MAX;
+
+	double joint_rotRARM_t = double(int(animation_frame*joint_rot_speed) % int(JOINT_MAX)) / JOINT_MAX;
+	joint_rotRARM += joint_rotRARM_t * JOINT_MIN + (1 - joint_rotRARM_t) * JOINT_MAX;
+
+	double joint_rotLLEG_t = double(int(animation_frame*joint_rot_speed) % int(JOINT_MAX)) / JOINT_MAX;
+	joint_rotLLEG += joint_rotLLEG_t * JOINT_MIN + (1 - joint_rotLLEG_t) * JOINT_MAX;
+
+	double joint_rotRLEG_t = double(int(animation_frame*joint_rot_speed) % int(JOINT_MAX)) / JOINT_MAX;
+	joint_rotRLEG += joint_rotRLEG_t * JOINT_MIN + (1 - joint_rotRLEG_t) * JOINT_MAX;
 
 	glui->sync_live();
-
-
-	//glutSetWindow(windowID);
+	glutSetWindow(windowID);
 	glutPostRedisplay();
 
 	animation_frame++;
@@ -134,7 +115,7 @@ void animateButton(int)
 void leftArm()
 {
 	glPushMatrix();
-	glRotatef(rotateLeftArm, 1.0f, 0.0f, 0.0f);
+	//glRotatef(rotateLeftArm, 1.0f, 0.0f, 0.0f);
 	glTranslatef(-1.0f, 0.1f, 0.0f);
 	glScalef(1.0f, 0.2f, 0.2f);
 	glutSolidCube(1.0);
@@ -144,7 +125,7 @@ void leftArm()
 void rightArm()
 {
 	glPushMatrix();
-	glRotatef(rotateRightArm, 1.0f, 0.0f, 0.0f);
+	//glRotatef(rotateRightArm, 1.0f, 0.0f, 0.0f);
 	glTranslatef(1.0f, 0.1f, 0.0f);
 	glScalef(1.0f, 0.2f, 0.2f);
 	glutSolidCube(1.0);
@@ -179,26 +160,11 @@ void body()
 void head()
 {
 	glPushMatrix();
-	glRotatef(rotateHead, 0.0f, 1.0f, 0.0f);
+	//glRotatef(rotateHead, 0.0f, 1.0f, 0.0f);
 	glTranslatef(0.0f, 0.75f, 0.0f);
 	glScalef(1.5f, 0.5f, 1.0f);
 	glutSolidCube(1.0);
 	glScalef(1.0f, 2.0f, 1.0f);
-	glPopMatrix();
-}
-
-void output(GLfloat x, GLfloat y, char *format, ...)
-{
-	va_list args;
-	char buffer[200], *p;
-
-	va_start(args, format);
-	vsprintf(buffer, format, args);
-	va_end(args);
-	glPushMatrix();
-	glTranslatef(x, y, 0);
-	for (p = buffer; *p; p++)
-		glutStrokeCharacter(GLUT_STROKE_ROMAN, *p);
 	glPopMatrix();
 }
 
@@ -218,13 +184,30 @@ void display(void)
 	glCallList(TEAPOT_MATERIAL);
 	head();
 	body();
+
+	glPushMatrix();
+	glRotatef((GLfloat)joint_rotLARM, 0.0, 1.0, 0.0);
 	leftArm();
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef((GLfloat)joint_rotRARM, 0.0, 1.0, 0.0);
 	rightArm();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glRotatef((GLfloat)joint_rotLLEG, 1.0, 0.0, 0.0);
 	leftLeg();
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef((GLfloat)joint_rotRLEG, 1.0, 0.0, 0.0);
 	rightLeg();
 	glPopMatrix();
-	glPushMatrix();
 
+	glPopMatrix();
+	glPushMatrix();
 	glPopMatrix();
 	glPopMatrix();
 	glPushAttrib(GL_ENABLE_BIT);
@@ -288,38 +271,6 @@ void material(int dlist, GLfloat * ambient, GLfloat * diffuse,
 	glMaterialf(GL_FRONT, GL_SHININESS, shininess);
 	glEndList();
 }
-
-char * material_select(int object, int value)
-{
-	glutPostRedisplay();
-	switch (value) {
-	case BRASS:
-		material(object, brass_ambient,
-			brass_diffuse, brass_specular, brass_shininess);
-		return "brass";
-	case RED_PLASTIC:
-		material(object, red_plastic_ambient, red_plastic_diffuse,
-			red_plastic_specular, red_plastic_shininess);
-		return "red plastic";
-	case EMERALD:
-		material(object, emerald_ambient, emerald_diffuse,
-			emerald_specular, emerald_shininess);
-		return "emerald";
-	case SLATE:
-		material(object, slate_ambient, slate_diffuse,
-			slate_specular, slate_shininess);
-		return "slate";
-	}
-	return NULL; /* avoid bogus warning! */
-}
-
-void main_menu_select(int value)
-{
-	if (value == 666)
-		exit(0);
-	glShadeModel(shade_model = value);
-	glutPostRedisplay();
-}
 void resize(int x, int y)
 {
 	double aspect = WINDOW_WIDTH / WINDOW_HEIGHT;
@@ -355,7 +306,7 @@ void initGlui()
 {
 	GLUI_Master.set_glutIdleFunc(NULL);
 
-	glutSetWindow(windowID);
+	//glutSetWindow(windowID);
 	// Create GLUI window
 	glui = GLUI_Master.create_glui("Glui Window", 0, Win[0] + 10, 0);
 
